36bd7732c0f6898f67f925892868566d66abd273
Kirill Kovalev <125643929+kirill-abblix@users.noreply.github.com>
2024-04-13 12:38:28 +0300

    TimeProvider moved to Common

--- Abblix.Oidc.Server/Common/TimeProvider.cs
+++ Abblix.Oidc.Server/Common/TimeProvider.cs
@@ -34,19 +34,42 @@ namespace System;
 
 /// <summary>
 /// Represents a clock that provides the current UTC time based on the system clock.
-/// This implementation is intended for use in versions of .NET earlier than 8.0, relying on the system's clock.
+/// This implementation is intended for use in environments or versions of .NET earlier than 8.0,
+/// where access to a more precise or configurable time source is not required. It abstracts the
+/// mechanism for obtaining time, allowing for more flexible testing or future changes to time
+/// acquisition strategies without altering dependent code.
 /// </summary>
 public abstract class TimeProvider
 {
     /// <summary>
     /// Gets the current UTC time directly from the system clock.
+    /// This abstract method must be implemented by subclasses to return the current UTC time,
+    /// allowing different strategies for time retrieval, such as fixed time for testing or
+    /// alternate time sources.
     /// </summary>
+    /// <returns>The current UTC time as a <see cref="DateTimeOffset"/>.</returns>
     public abstract DateTimeOffset GetUtcNow();
 
+    /// <summary>
+    /// Provides a singleton instance of the <see cref="TimeProvider"/> that retrieves the current time
+    /// using the system's default clock, specifically <see cref="DateTimeOffset.UtcNow"/>.
+    /// This standard implementation is thread-safe and efficient, suitable for general use across various
+    /// application components.
+    /// </summary>
     public static readonly TimeProvider System = new SystemTimeProvider();
 
+    /// <summary>
+    /// A private nested class that provides the system time. This class implements the
+    /// <see cref="TimeProvider.GetUtcNow"/> method using the system's clock.
+    /// </summary>
     private class SystemTimeProvider : TimeProvider
     {
+        /// <summary>
+        /// Returns the current UTC time from the system's clock.
+        /// This method directly accesses <see cref="DateTimeOffset.UtcNow"/> to provide the current time,
+        /// ensuring that time retrievals are fast and reflect the actual system time with no adjustments or modifications.
+        /// </summary>
+        /// <returns>The current UTC time as a <see cref="DateTimeOffset"/>.</returns>
         public override DateTimeOffset GetUtcNow() => DateTimeOffset.UtcNow;
     }
 }
